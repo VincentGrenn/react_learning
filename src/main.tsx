532afac515eb4463b6a0dfd4356f49ae8ab5e3c4
VincentGrenn <s_vincent.grenn@ousd.org>
2025-08-24 04:36:11 +0000

    another small little fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/app.js";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>
-)
+if (typeof(document) !== null) {    
+    ReactDOM.createRoot(document.getElementById("root")).render(
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>
+    )
+}

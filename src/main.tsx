015dbe6914ee91aaf21e16a1a24b3938d98ba745
VincentGrenn <s_vincent.grenn@ousd.org>
2025-08-24 04:37:21 +0000

    again.. fixed it

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/app.js";
 
-if (typeof(document) !== null) {    
+if (typeof(document) !== undefined) {    
     ReactDOM.createRoot(document.getElementById("root")).render(
         <React.StrictMode>
             <App/>

bf4bae924afae8e0e16c5f69a37e4c2038f4376f
VincentGrenn <s_vincent.grenn@ousd.org>
2025-08-24 07:37:44 +0000

    added more stuff

--- react_learning/src/main.tsx
+++ react_learning/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/app.js";
+import "./fonts/Poppins-Regular.ttf";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>

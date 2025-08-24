bc8a8984b6c148526c5880b93741d4ed329260f9
VincentGrenn <s_vincent.grenn@ousd.org>
2025-08-24 06:06:01 +0000

    more fixes

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,37 @@
 import http from "http";
+import fs from "fs";
+import url from "url";
+import path from "path";
+import mimetypes from "mime-types";
 
 const PORT = 5000;
 
+function ServeStaticFile(req: http.IncomingMessage, res: http.ServerResponse) {
+    const ParsedURL = url.parse(req.url, true);
+    let FileName = ParsedURL.path.replace(/^\/+|\/+$/g, "");
+
+    if (FileName === "") FileName = "index.html";
+
+    const FilePath = import.meta.dirname.replace("/server", "") + "/client/src/" + FileName;
+
+    fs.readFile(FilePath, (err, data) => {
+        if (err) {
+            res.writeHead(404, {"Content-Type": "application/json"});
+            res.end(JSON.stringify({
+                message: `Unable to locate file "${FileName}"!`
+            }));
+            return;
+        }
+        res.setHeader("X-Content-Type-Options", "nosniff");
+        const Mime = mimetypes.lookup(FileName) as string;
+        res.writeHead(200, {"Content-Type": Mime});
+        res.end(data);
+    })
+}
+
 const Server = http.createServer((req, res) => {
     if (req.url === "/" || req.url === "/home" && req.method === "GET") {
-        res.writeHead(200, {"Content-Type": "application/json"});
-        res.end(JSON.stringify({
-            message: "OK",
-        }));
+        ServeStaticFile(req, res);
     }
 });
 

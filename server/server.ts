4e3d2ac2f3edbb541d03927d6773251322ebd59c
VincentGrenn <s_vincent.grenn@ousd.org>
2025-08-24 05:51:17 +0000

    i keep making too many errors

--- server/server.ts
+++ server/server.ts
@@ -4,10 +4,10 @@ const PORT = 5000;
 
 const Server = http.createServer((req, res) => {
     if (req.url === "/" || req.url === "/home" && req.method === "GET") {
-        res.writeHead(200, JSON.stringify({
+        res.writeHead(200, {"Content-Type": "application/json"});
+        res.end(JSON.stringify({
             message: "OK",
         }));
-        res.end();
     }
 });
 
